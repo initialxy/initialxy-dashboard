25c0f5dab7241974d799e6bcf8e4af7aa10f72d9
Xingchen Yu <initialxy@gmail.com>
2022-03-05 22:52:01 -0800

    Temporary fix to make type checker happy

--- thrift/types.thrift
+++ thrift/types.thrift
@@ -1,5 +1,5 @@
 struct Task {
   1: required string desc,
   2: required i16 pri, // 0 being highest priority
-  3: required i64 time, // UNIX timestamp. 0 to indicate unset
+  3: required double time, // UNIX timestamp. 0 to indicate unset
 }
