f0ace341e4987a2f8a6e82b82d85a13b69af0694
Xingchen Yu <initialxy@gmail.com>
2022-03-11 16:20:28 -0800

    Allow proof_of_concept script to loop

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import sys
 import os
+import sys
+
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
+import io
 import logging
-import epaper
 import time
-from PIL import Image,ImageDraw,ImageFont
 import traceback
+from datetime import datetime
+
+import epaper
+from PIL import Image, ImageDraw, ImageFont
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import io
 
 logging.basicConfig(level=logging.DEBUG)
+driver = None
+epd = None
 
 try:
     logging.info("epd7in5_V2 Demo")
@@ -36,20 +41,20 @@ try:
 
     driver = webdriver.Chrome(options=chrome_options, desired_capabilities=caps)
     driver.get("http://localhost:8000")
-    for i in range(3):
+    while True:
       img_binary = driver.get_screenshot_as_png()
       image = Image.open(io.BytesIO(img_binary)) 
       epd.display(epd.getbuffer(image))
-      time.sleep(10)
-
-    driver.quit()
-    logging.info("Goto Sleep...")
-    epd.Clear()
-    epd.sleep()
+      time.sleep(70 - (datetime.now().timestamp() % 60))
     
 except IOError as e:
     logging.info(e)
     
-except KeyboardInterrupt:    
+except KeyboardInterrupt:
     logging.info("ctrl + c:")
+    if driver:
+        driver.quit()
+    if epd:
+        epd.Clear()
+        epd.sleep()
     exit()
