35b00d0b77ad353eb8ba7d627a8db3d463217017
Xingchen Yu <initialxy@gmail.com>
2022-03-11 23:18:03 -0800

    Added display refresh loop as part of the Tornado event loop

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 import os
+import time
 
 import tornado.ioloop
 import tornado.web
 
 from handlers.handlers import ConfigHandler, StocksHandler, TasksHandler
 from utils.config import get_config
+from utils.display import Display
 
 CONFIG = get_config()
 
@@ -27,7 +29,24 @@ def make_app() -> tornado.web.Application:
   )
 
 
+def refresh_display():
+  Display.refresh()
+  tornado.ioloop.IOLoop.current().call_later(
+    70 - (time.time() % 60),
+    refresh_display,
+  )
+
+
+def init_display():
+  Display.init_display()
+  tornado.ioloop.IOLoop.current().call_later(15, refresh_display)
+
+
 if __name__ == "__main__":
   app = make_app()
   app.listen(CONFIG.port)
-  tornado.ioloop.IOLoop.current().start()
+  tornado.ioloop.IOLoop.current().call_later(1, init_display)
+  try:
+    tornado.ioloop.IOLoop.current().start()
+  except KeyboardInterrupt:
+    Display.close()
---
+++ utils/display.py
@@ -0,0 +1,62 @@
+import io
+from typing import Any, Optional
+
+import epaper
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chromium.webdriver import ChromiumDriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+from utils.config import get_config
+
+CONFIG = get_config()
+
+
+class Display:
+  __epd: Any = None
+  __driver: Optional[ChromiumDriver] = None
+
+  @classmethod
+  def init_display(cls):
+    if not cls.__driver:
+      chrome_options = Options()
+      chrome_options.add_argument("headless")
+      chrome_options.add_argument("window-size=800x480")
+      chrome_options.add_argument("hide-scrollbars")
+
+      caps = DesiredCapabilities().CHROME
+      caps["pageLoadStrategy"] = "none"
+
+      cls.__driver = webdriver.Chrome(
+        options=chrome_options,
+        desired_capabilities=caps,
+      )
+      cls.__driver.get("http://localhost:" + str(CONFIG.port))
+
+    if not cls.__epd and not CONFIG.save_image:
+      cls.__epd = epaper.epaper(CONFIG.epaper_model).EPD()
+      cls.__epd.init()
+
+  @classmethod
+  def refresh(cls):
+    cls.init_display()
+
+    img_binary = cls.__driver.get_screenshot_as_png()
+    image = Image.open(io.BytesIO(img_binary))
+
+    if CONFIG.save_image:
+      image.save(CONFIG.save_image)
+    else:
+      cls.__epd.display(cls.__epd.getbuffer(image))
+      cls.__epd.sleep()
+
+  @classmethod
+  def close(cls):
+    if cls.__driver:
+      cls.__driver.quit()
+      cls.__driver = None
+    if cls.__epd:
+      cls.__epd.Clear()
+      cls.__epd.sleep()
+      cls.__epd = None
