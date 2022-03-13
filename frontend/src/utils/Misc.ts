d7a1faef8df985aba3a37898f783566e5ee04fc4
Xingchen Yu <initialxy@gmail.com>
2022-03-12 22:33:01 -0800

    Added a debounce function

--- frontend/src/utils/Misc.ts
+++ frontend/src/utils/Misc.ts
@@ -1,7 +1,7 @@
 export function emptyFunc(): void { }
 
 export async function sleep(ms: number): Promise<void> {
-  return new Promise((resolve, _) => setTimeout(resolve, ms));
+  return new Promise((resolve, _) => window.setTimeout(resolve, ms));
 }
 
 export function nullthrows<T>(v?: T): T {
@@ -21,10 +21,23 @@ export function clx(clsDef: { [cls: string]: boolean }): string {
   return classes.join(" ");
 }
 
-export function stx(styleDef: {[nane: string]: string}): string {
+export function stx(styleDef: { [nane: string]: string }): string {
   const styles = [];
   for (const p in styleDef) {
     styles.push(`${p}: ${styleDef[p]};`);
   }
   return styles.join(" ");
+}
+
+export function debounce(fn: () => void, ms: number): () => void {
+  let scheduleID = 0;
+  return () => {
+    if (scheduleID !== 0) {
+      window.clearTimeout(scheduleID);
+    }
+    scheduleID = window.setTimeout(() => {
+      scheduleID = 0;
+      fn();
+    }, ms);
+  };
 }
